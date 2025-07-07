3207baf1a4eb8db500a1fdbb09cb8c9fa9e91a69
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-07-07 07:59:56 +0545

    form data

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Main = () => {
+  const [ingredients, setIngredients] = useState([]);
+
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredients");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+    console.log(ingredients);
+  }
   return (
     <main>
-      <form className="add-ingredient">
+      <form className="add-ingredient" action={addIngredient}>
         <input
           type="text"
           aria-label="Add ingredient"
           placeholder="e.g. oregano"
+          name="ingredients"
         />
-        <button onClick={}>Add ingredient</button>
+        <button>Add ingredient</button>
       </form>
+      <ul>
+        {ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
     </main>
   );
 };
